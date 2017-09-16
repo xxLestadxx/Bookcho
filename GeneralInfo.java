e6f4e557ed31c3656706fe256ff27bb7283d031e
Yordan Gospodinov <j.v.gospodinov@gmail.com>
2017-09-16 17:04:38 +0100

    Add files via upload

---
+++ GeneralInfo.java
@@ -0,0 +1,35 @@
+public class GeneralInfo {
+
+	private String titleName;
+	private String genreName;
+	private String authorName;
+	private String edition;
+	private int year = 0;
+
+	GeneralInfo(String title, String genre, String author, int year) {
+		titleName = title;
+		genreName = genre;
+		authorName = author;
+		this.year = year;
+	}
+
+	public String getTitleName() {
+		return titleName;
+	}
+
+	public String getGenreName() {
+		return genreName;
+	}
+
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public String getEdition(){
+		return edition;
+	}
+	public int getYear() {
+		return year;
+	}
+
+}
