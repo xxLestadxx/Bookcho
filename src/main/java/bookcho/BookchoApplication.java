00b1155dca1c7ef60d3c3077a43b9cc9e27a46b5
Yordan <j.v.gospodinov@gmail.com>
2023-09-06 14:56:52 +0300

    revive

---
+++ src/main/java/bookcho/BookchoApplication.java
@@ -0,0 +1,13 @@
+package bookcho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookchoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookchoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bookcho/BookchoApplicationTests.java
@@ -0,0 +1,13 @@
+package bookcho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookchoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
